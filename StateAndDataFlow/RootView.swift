b26eb0a43c2b639a3eaae0c9a63f2d5ae8e98df4
Yaroslav Malygin <ya.malygin@team.superjob.ru>
2025-02-04 16:35:59 +0300

    Initial Commit

---
+++ StateAndDataFlow/RootView.swift
@@ -0,0 +1,25 @@
+//
+//  RootView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 22.04.2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @EnvironmentObject private var loginViewVM: LoginViewViewModel
+    
+    var body: some View {
+        if loginViewVM.isLoggedIn {
+            ContentView()
+        } else {
+            LoginView()
+        }
+    }
+}
+
+#Preview {
+    RootView()
+        .environmentObject(LoginViewViewModel())
+}
